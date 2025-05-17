8565d024165e0d2608f8774d18c053b09168b347
Gabriel Kaizer de Menezes <g.menezes@edu.pucrs.br>
2025-05-17 14:01:09 -0300

    Update: adjust after merge

--- src/Protocol.cpp
+++ src/Protocol.cpp
@@ -41,11 +41,7 @@ SigmaProtocol::SigmaProtocol(int numDevices) : numDevices(numDevices) {
 
     if (std::getline(file, linha)) {
         tokenTimeout = std::stoi(linha);
-<<<<<<< HEAD
         printf("Token Timeout: %d\n", tokenTimeout);
-=======
-        printf("Token Timeout: %d\n", tokenTimeout);
->>>>>>> origin/kaizer-branch
     }
 
     if (std::getline(file, linha)) {
